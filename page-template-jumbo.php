3267276676149a0d3e66e0c7e6be3af5eb1a4496
m-e-h <info@martyhelmick.com>
2014-08-14 14:04:30 -0400

    set feature image as slider backdrop

--- page-template-jumbo.php
+++ page-template-jumbo.php
@@ -17,6 +17,18 @@ get_header(); // Loads the header.php template. ?>
 
 						<?php the_content(); ?>
 
+						<?php if (has_post_thumbnail( $post->ID ) ): ?>
+<?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+
+						<style type="text/css" id="slider-background-css">.flexslider-background { 
+							background: url(<?php echo $image[0]; ?>) no-repeat center center fixed; 
+  						-webkit-background-size: cover;
+  						-moz-background-size: cover;
+  						-o-background-size: cover;
+  						background-size: cover;
+  						 }</style>
+  						 <?php endif; ?>
+
 				<article <?php hybrid_attr( 'post' ); ?>>
 
 					<?php hybrid_get_sidebar( 'featured' ); // Loads the sidebar/subsidiary.php template. ?>
