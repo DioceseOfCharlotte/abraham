43d0f3b01b52e011a41dbc9d4a609569fae2df3d
m-e-h <info@martyhelmick.com>
2014-08-15 15:25:43 -0400

    version bump

--- page-template-jumbo.php
+++ page-template-jumbo.php
@@ -15,7 +15,6 @@
 
       <?php the_content(); ?>
 
-
         <?php if (has_post_thumbnail( $post->ID ) ): ?>
 <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
 
