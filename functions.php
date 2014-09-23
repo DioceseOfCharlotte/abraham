63f007431d68d978b62af763a642f9ca2ca26803
m-e-h <info@martyhelmick.com>
2014-09-23 09:29:30 -0400

    Clean up Functions

--- functions.php
+++ functions.php
@@ -9,7 +9,7 @@
  * Set the content width based on the theme's design and stylesheet.
  */
 if ( ! isset( $content_width ) ) {
-	$content_width = 640; /* pixels */
+	$content_width = 1100; /* pixels */
 }
 
 if ( ! function_exists( 'abraham_setup' ) ) :
@@ -34,26 +34,6 @@ function abraham_setup() {
 	add_theme_support( 'automatic-feed-links' );
 
 	/*
-	 * Enable support for Post Thumbnails on posts and pages.
-	 *
-	 * @link http://codex.wordpress.org/Function_Reference/add_theme_support#Post_Thumbnails
-	 */
-	//add_theme_support( 'post-thumbnails' );
-
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' => __( 'Primary Menu', 'abraham' ),
-	) );
-
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form', 'comment-form', 'comment-list', 'gallery', 'caption',
-	) );
-
-	/*
 	 * Enable support for Post Formats.
 	 * See http://codex.wordpress.org/Post_Formats
 	 */
@@ -71,43 +51,9 @@ endif; // abraham_setup
 add_action( 'after_setup_theme', 'abraham_setup' );
 
 /**
- * Register widget area.
- *
- * @link http://codex.wordpress.org/Function_Reference/register_sidebar
- */
-function abraham_widgets_init() {
-	register_sidebar( array(
-		'name'          => __( 'Sidebar', 'abraham' ),
-		'id'            => 'sidebar-1',
-		'description'   => '',
-		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
-		'after_widget'  => '</aside>',
-		'before_title'  => '<h1 class="widget-title">',
-		'after_title'   => '</h1>',
-	) );
-}
-add_action( 'widgets_init', 'abraham_widgets_init' );
-
-/**
- * Enqueue scripts and styles.
- */
-function abraham_scripts() {
-	wp_enqueue_style( 'abraham-style', get_stylesheet_uri() );
-
-	wp_enqueue_script( 'abraham-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
-
-	wp_enqueue_script( 'abraham-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
-
-	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
-		wp_enqueue_script( 'comment-reply' );
-	}
-}
-add_action( 'wp_enqueue_scripts', 'abraham_scripts' );
-
-/**
  * Implement the Custom Header feature.
  */
-//require get_template_directory() . '/inc/custom-header.php';
+require get_template_directory() . '/inc/custom-header.php';
 
 /**
  * Custom template tags for this theme.
@@ -123,6 +69,11 @@ require get_template_directory() . '/inc/extras.php';
  * Customizer additions.
  */
 require get_template_directory() . '/inc/customizer.php';
+
+/**
+ * Abraham additions.
+ */
+require get_template_directory() . '/inc/theme.php';
 
 /**
  * Load Jetpack compatibility file.
--- inc/theme.php
+++ inc/theme.php
@@ -7,7 +7,7 @@ add_action( 'init', 'abraham_register_image_sizes', 5 );
 add_action( 'init', 'abraham_register_menus', 5 );
 
 /* Register sidebars. */
-add_action( 'widgets_init', 'abraham_register_sidebars', 5 );
+add_action( 'widgets_init', 'abraham_widgets_init', 5 );
 
 /* Add custom scripts. */
 add_action( 'wp_enqueue_scripts', 'abraham_enqueue_scripts', 5 );
@@ -16,7 +16,7 @@ add_action( 'wp_enqueue_scripts', 'abraham_enqueue_scripts', 5 );
 add_action( 'wp_enqueue_scripts', 'abraham_enqueue_styles', 5 );
 
 /**
- * Registers custom image sizes for the theme. 
+ * Registers custom image sizes for the theme.
  *
  * @since  1.0.0
  * @access public
@@ -39,66 +39,63 @@ function abraham_register_image_sizes() {
 function abraham_register_menus() {
 	register_nav_menu( 'primary',    _x( 'Primary',    'nav menu location', 'abraham' ) );
 	register_nav_menu( 'secondary',  _x( 'Secondary',  'nav menu location', 'abraham' ) );
-	register_nav_menu( 'social', 		 _x( 'Social', 'nav menu location', 'abraham' ) );
+	register_nav_menu( 'social', 		 _x( 'Social',     'nav menu location', 'abraham' ) );
 }
 
 /**
- * Registers sidebars.
+ * Register widget area.
  *
- * @since  1.0.0
- * @access public
- * @return void
+ * @link http://codex.wordpress.org/Function_Reference/register_sidebar
  */
-function abraham_register_sidebars() {
+function abraham_widgets_init() {
 
-	hybrid_register_sidebar(
-		array(
-			'id'          => 'primary',
-			'name'        => _x( 'Primary Sidebar', 'sidebar', 'abraham' ),
-			'description' => __( 'Typically on the screens left side.', 'abraham' ),
-			'before_title'  => '<h2 class="primary-title">',
-			'after_title'   => '</h2>',
-			'before_widget' => '<section id="%1$s" class="primary widget %2$s">',
-			'after_widget'  => '</section>',
-		)
-	);
+	hybrid_register_sidebar( array(
+		'id'            => 'sidebar-1',
+		'name'          => _x( 'Primary Sidebar', 'sidebar', 'abraham' ),
+		'description'   => __( 'Typically on the screens left side.', 'abraham' ),
+		'before_title'  => '<h2 class="primary-title">',
+		'after_title'   => '</h2>',
+		'before_widget' => '<section id="%1$s" class="primary widget %2$s">',
+		'after_widget'  => '</section>',
+	) );
 
-	hybrid_register_sidebar(
-		array(
-			'id'          => 'secondary',
-			'name'        => _x( 'Secondary Sidebar', 'sidebar', 'abraham' ),
-			'description' => __( 'Typically on the screens right side.', 'abraham' ),
-			'before_title'  => '<h2 class="secondary-title">',
-			'after_title'   => '</h2>',
-			'before_widget' => '<section id="%1$s" class="secondary widget %2$s">',
-			'after_widget'  => '</section>',
-		)
-	);
+	hybrid_register_sidebar( array(
+		'name'          => _x( 'Secondary Sidebar', 'sidebar', 'abraham' ),
+		'id'            => 'sidebar-2',
+		'description'   => '',
+		'before_widget' => '<aside id="%1$s" class="widget %2$s">',
+		'after_widget'  => '</aside>',
+		'before_title'  => '<h2 class="widget-title">',
+		'after_title'   => '</h2>',
+	) );
 
-		hybrid_register_sidebar(
-		array(
-			'id'          => 'subsidiary',
-			'name'        => _x( 'Footer Widget Area', 'sidebar', 'abraham' ),
-			'description' => __( 'Footer widgets.', 'abraham' ),
-			'before_title'  => '<h2 class="subsidiary-title">',
-			'after_title'   => '</h2>',
-			'before_widget' => '<section id="%1$s" class="subsidiary widget %2$s">',
-			'after_widget'  => '</section>',
-		)
-	);
-
-		hybrid_register_sidebar(
-		array(
-			'id'            => 'featured',
-			'name'          => _x( 'Home Features', 'sidebar', 'abraham' ),
-			'description'   => __( 'Add services or features you\'d like to highlight.', 'abraham' ),
-			'before_title'  => '<h2 class="feature-title">',
-			'after_title'   => '</h2>',
-			'before_widget' => '<section id="%1$s" class="%1$s feature widget %2$s">',
-			'after_widget'  => '</section>',
-		)
-	);
+		hybrid_register_sidebar( array(
+		'id'            => 'sidebar-3',
+		'name'          => _x( 'Footer Widget Area', 'sidebar', 'abraham' ),
+		'description'   => __( 'Footer widgets.', 'abraham' ),
+		'before_title'  => '<h2 class="subsidiary-title">',
+		'after_title'   => '</h2>',
+		'before_widget' => '<section id="%1$s" class="subsidiary widget %2$s">',
+		'after_widget'  => '</section>',
+	) );
 }
+
+/**
+ * Enqueue scripts and styles.
+ */
+function abraham_scripts() {
+	wp_enqueue_style( 'abraham-style', get_stylesheet_uri() );
+
+	wp_enqueue_script( 'abraham-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20120206', true );
+
+	wp_enqueue_script( 'abraham-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20130115', true );
+
+	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
+		wp_enqueue_script( 'comment-reply' );
+	}
+}
+add_action( 'wp_enqueue_scripts', 'abraham_scripts' );
+
 
 /**
  * Load scripts for the front end.
