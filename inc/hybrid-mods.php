45e757ba7180f4c3d94d33fc837f4d69bd73fb16
m-e-h <info@martyhelmick.com>
2014-09-29 18:22:48 -0400

    spaces to tabs

--- inc/hybrid-mods.php
+++ inc/hybrid-mods.php
@@ -7,76 +7,76 @@
 class Doc_Attributes {
 
 	/* Attributes for major structural elements. */
-  public $body        					= ' logo';
-  public $header            		= ' app-bar';
-  public $footer     						= ' doc-card layout__item';
-  public $content        				= ' logo';
-  public $sidebar            		= ' app-bar';
-  public $menu     							= ' doc-card';
+  public $body        				  = ' logo';
+  public $header            	  = ' app-bar';
+  public $footer     					  = ' doc-card layout__item';
+  public $content        			  = ' logo';
+  public $sidebar            	  = ' app-bar';
+  public $menu     						  = ' doc-card';
 
 	/* Header attributes. */
-  public $branding        			= ' logo';
+  public $branding        		  = ' logo';
   public $site_title            = ' app-bar';
-  public $site_description     	= ' doc-card  sm-1-2 lg-1-3';
+  public $site_description      = ' doc-card  sm-1-2 lg-1-3';
 
 	/* Loop attributes. */
-  public $loop_meta        			= ' logo';
+  public $loop_meta        		  = ' logo';
   public $loop_title            = ' app-bar';
-  public $loop_description     	= ' doc-card  sm-1-2 lg-1-3';
+  public $loop_description      = ' doc-card  sm-1-2 lg-1-3';
 
 	/* Post-specific attributes. */
-  public $post        					= ' logo';
+  public $post        				  = ' logo';
   public $entry_title           = ' app-bar';
-  public $entry_author     			= ' doc-card layout__item lg-1-3';
+  public $entry_author     		  = ' doc-card layout__item lg-1-3';
   public $entry_published       = ' logo';
   public $entry_content         = ' app-bar';
-  public $entry_summary     		= ' doc-card palm-1-1 sm-1-2 lg-1-3';
-  public $entry_terms      		 	= ' doc-card palm-1-1 sm-1-2 lg-1-3';
+  public $entry_summary     	  = ' doc-card palm-1-1 sm-1-2 lg-1-3';
+  public $entry_terms      		  = ' doc-card palm-1-1 sm-1-2 lg-1-3';
 
 	/* Comment specific attributes. */
-  public $comment            	 	= ' app-bar';
-  public $comment_author       	= ' logo';
-  public $comment_published    	= ' app-bar';
-  public $comment_permalink    	= ' doc-card layout__item lg-1-3';
-  public $comment_content      	= ' logo';
+  public $comment            	  = ' app-bar';
+  public $comment_author        = ' logo';
+  public $comment_published     = ' app-bar';
+  public $comment_permalink     = ' doc-card layout__item lg-1-3';
+  public $comment_content       = ' logo';
 
 
 
   public function __construct() {
 
 /* Attributes for major structural elements. */
-add_filter( 'hybrid_attr_body',    					array( $this, 'body' 									) );
-add_filter( 'hybrid_attr_header',  					array( $this, 'header' 								) );
-add_filter( 'hybrid_attr_footer',  					array( $this, 'footer' 								) );
-add_filter( 'hybrid_attr_content', 					array( $this, 'content' 							) );
-add_filter( 'hybrid_attr_sidebar',  				array( $this, 'sidebar' 								) );
-add_filter( 'hybrid_attr_menu', 						array( $this, 'menu' 							) );
+add_filter( 'hybrid_attr_body',    					array( $this, 'body' ) );
+add_filter( 'hybrid_attr_header',  					array( $this, 'header' ) );
+add_filter( 'hybrid_attr_footer',  					array( $this, 'footer' ) );
+add_filter( 'hybrid_attr_content', 					array( $this, 'content' ) );
+add_filter( 'hybrid_attr_sidebar',  				array( $this, 'sidebar' ) );
+add_filter( 'hybrid_attr_menu', 						array( $this, 'menu' ) );
 
 /* Header attributes. */
-add_filter( 'hybrid_attr_branding',         array( $this, 'branding' 							) );
-add_filter( 'hybrid_attr_site-title',       array( $this, 'site_title' 						) );
-add_filter( 'hybrid_attr_site-description', array( $this, 'site_description' 			) );
+add_filter( 'hybrid_attr_branding',         array( $this, 'branding' ) );
+add_filter( 'hybrid_attr_site-title',       array( $this, 'site_title' ) );
+add_filter( 'hybrid_attr_site-description', array( $this, 'site_description' ) );
 
 /* Loop attributes. */
-add_filter( 'hybrid_attr_loop-meta',        array( $this, 'loop_meta' 						) );
-add_filter( 'hybrid_attr_loop-title',       array( $this, 'loop_title' 						) );
-add_filter( 'hybrid_attr_loop-description', array( $this, 'loop_description' 			) );
+add_filter( 'hybrid_attr_loop-meta',        array( $this, 'loop_meta' ) );
+add_filter( 'hybrid_attr_loop-title',       array( $this, 'loop_title' ) );
+add_filter( 'hybrid_attr_loop-description', array( $this, 'loop_description' ) );
 
 /* Post-specific attributes. */
-add_filter( 'hybrid_attr_post',            	array( $this, 'post' 									) );
-add_filter( 'hybrid_attr_entry-title',     	array( $this, 'entry_title' 					) );
-add_filter( 'hybrid_attr_entry-author',    	array( $this, 'entry_author' 					) );
-add_filter( 'hybrid_attr_entry-published', 	array( $this, 'entry_published' 			) );
-add_filter( 'hybrid_attr_entry-content',   	array( $this, 'entry_content' 				) );
-add_filter( 'hybrid_attr_entry-summary',   	array( $this, 'entry_summary' 				) );
-add_filter( 'hybrid_attr_entry-terms',     	array( $this, 'entry_terms' 					) );
+add_filter( 'hybrid_attr_post',            	array( $this, 'post' ) );
+add_filter( 'hybrid_attr_entry-title',     	array( $this, 'entry_title' ) );
+add_filter( 'hybrid_attr_entry-author',    	array( $this, 'entry_author' ) );
+add_filter( 'hybrid_attr_entry-published', 	array( $this, 'entry_published' ) );
+add_filter( 'hybrid_attr_entry-content',   	array( $this, 'entry_content' ) );
+add_filter( 'hybrid_attr_entry-summary',   	array( $this, 'entry_summary' ) );
+add_filter( 'hybrid_attr_entry-terms',     	array( $this, 'entry_terms' ) );
 
 /* Comment specific attributes. */
-add_filter( 'hybrid_attr_comment',           array( $this, 'comment' 							) );
-add_filter( 'hybrid_attr_comment-author',    array( $this, 'comment_author' 			) );
-add_filter( 'hybrid_attr_comment-published', array( $this, 'comment_published' 		) );
-add_filter( 'hybrid_attr_comment-permalink', array( $this, 'comment_permalink' 		) );
-add_filter( 'hybrid_attr_comment-content',   array( $this, 'comment_content' 			) );
+add_filter( 'hybrid_attr_comment',           array( $this, 'comment' ) );
+add_filter( 'hybrid_attr_comment-author',    array( $this, 'comment_author' ) );
+add_filter( 'hybrid_attr_comment-published', array( $this, 'comment_published' ) );
+add_filter( 'hybrid_attr_comment-permalink', array( $this, 'comment_permalink' ) );
+add_filter( 'hybrid_attr_comment-content',   array( $this, 'comment_content' ) );
 }
 
 
