14d975b99d293223f282446a93f5f9fa31c7a5df
m-e-h <info@martyhelmick.com>
2014-11-17 20:38:22 -0500

    add Stargazer files

---
+++ misc/loop-nav.php
@@ -0,0 +1,17 @@
+<?php if ( is_singular( 'post' ) ) : ?>
+
+	<div class="loop-nav">
+		<?php previous_post_link( '<div class="prev">' . __( 'Previous Post: %link', 'abraham' ) . '</div>', '%title' ); ?>
+		<?php next_post_link(     '<div class="next">' . __( 'Next Post: %link',     'abraham' ) . '</div>', '%title' ); ?>
+	</div><!-- .loop-nav -->
+
+<?php elseif ( is_home() || is_archive() || is_search() ) : // If viewing the blog, an archive, or search results. ?>
+
+	<?php loop_pagination(
+		array( 
+			'prev_text' => _x( '&larr; Previous', 'posts navigation', 'abraham' ), 
+			'next_text' => _x( 'Next &rarr;',     'posts navigation', 'abraham' )
+		) 
+	); ?>
+
+<?php endif; // End check for type of page being viewed. ?>
