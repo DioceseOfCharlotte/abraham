f435e3ec8a28859a9d923f2c6ede28d0e57b0fde
m-e-h <info@martyhelmick.com>
2015-01-20 17:57:46 -0500

    add meta to dev posts

--- inc/template-actions.php
+++ inc/template-actions.php
@@ -3,7 +3,7 @@
 // add_action( 'tha_entry_before', 'abraham_do_format_icon' );
 
 
-
+add_action( 'tha_entry_top', 'abraham_do_byline' );
 
 
 
@@ -14,4 +14,14 @@ function abraham_do_format_icon() {
 	<span class="format-icon--wrap"><?php abraham_format_svg(); ?></span>
 	</span></a>
 	<?php
+}
+
+
+function abraham_do_byline() {
+
+if ( is_category( 'development' ) ) : ?>
+	<time <?php hybrid_attr( 'entry-published' ); ?>><?php echo get_the_date(); ?></time>
+	<span <?php hybrid_attr( 'entry-author' ); ?>><?php the_author_posts_link(); ?></span>
+<?php
+endif;
 }
