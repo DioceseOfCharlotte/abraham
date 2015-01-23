80b4d3e4211b0c22af885e9f41934ee8d330811f
m-e-h <info@martyhelmick.com>
2015-01-23 12:52:45 -0500

    customize theme-layouts menu

--- functions.php
+++ functions.php
@@ -57,9 +57,9 @@ function abraham_setup() {
 
 	/* Theme layouts. */
 	add_theme_support( 'theme-layouts', array(
-			'single-column'    => __( 'Single Column', 'platform' ),
-			'sidebar-right'  => __( 'Sidebar Right', 'platform' ),
-			'sidebar-left'  => __( 'Sidebar Left', 'platform' )
+			'single-column'    => __( 'Single Column', 'abraham' ),
+			'sidebar-right'  => __( 'Sidebar Right', 'abraham' ),
+			'sidebar-left'  => __( 'Sidebar Left', 'abraham' )
 		),
 		array( 'default' => 'sidebar-right' )
 	);
--- inc/customizer.php
+++ inc/customizer.php
@@ -24,3 +24,91 @@ function abraham_customize_preview_js() {
 	wp_enqueue_script( 'abraham_customizer', get_template_directory_uri() . '/js/customizer.js', array( 'customize-preview' ), '20130508', true );
 }
 add_action( 'customize_preview_init', 'abraham_customize_preview_js' );
+
+
+
+function abraham_customize_css() {
+?>
+<style type="text/css">
+#customize-control-theme-layout-control input[value=single-column]:before {
+	content: "\f134";
+}
+#customize-control-theme-layout-control input[value=sidebar-right]:before {
+	content: "\f135";
+}
+#customize-control-theme-layout-control input[value=sidebar-left]:before {
+	content: "\f136";
+}
+#customize-control-theme-layout-control input[type=radio] {
+	font-family: dashicons;
+	font-size: 32px;
+  	margin-right: 20px;
+	color: #ddd;
+	border: 0;
+	line-height: 0;
+	height: 0;
+	width: 0;
+}
+
+#customize-control-theme-layout-control input[type=radio]:checked:before {
+	color: #888;
+	text-indent: 0;
+	-webkit-border-radius: 0;
+  border-radius: 0;
+  font-size: 32px;
+  width: 0;
+  height: 0;
+   margin: 0;
+  line-height: 0;
+  background: none;
+}
+</style>
+<?php
+}
+add_action( 'customize_controls_init', 'abraham_customize_css' );
+
+function abraham_admin_styles() {
+?>
+<style type="text/css">
+#theme-layouts-post-meta-box input[value=default]:before {
+	content: "\f111";
+}
+#theme-layouts-post-meta-box input[value=single-column]:before {
+	content: "\f134";
+}
+#theme-layouts-post-meta-box input[value=sidebar-right]:before {
+	content: "\f135";
+}
+#theme-layouts-post-meta-box input[value=sidebar-left]:before {
+	content: "\f136";
+}
+#theme-layouts-post-meta-box input[type=radio] {
+	font-family: dashicons;
+	font-size: 32px;
+  	margin-right: 20px;
+	color: #ddd;
+	border: 0;
+	line-height: 0;
+	height: 0;
+	width: 0;
+}
+
+#theme-layouts-post-meta-box input[type=radio]:checked:before {
+	color: #888;
+	text-indent: 0;
+	-webkit-border-radius: 0;
+  border-radius: 0;
+  font-size: 32px;
+  width: 0;
+  height: 0;
+   margin: 0;
+  line-height: 0;
+  background: none;
+}
+#theme-layouts-post-meta-box li {
+  margin-bottom: 15px;
+}
+</style>
+<?php
+}
+add_action( 'admin_print_styles', 'abraham_admin_styles' );
