d9212a602bc65ebba386d025e54dbf53b8efc00f
m-e-h <info@martyhelmick.com>
2015-02-05 15:31:31 -0500

    add comments

--- inc/customizer/mods.php
+++ inc/customizer/mods.php
@@ -5,8 +5,15 @@
  * @package Customizer Library Demo
  */
 
+
+add_action( 'wp_enqueue_scripts', 'abraham_custom_fonts' );
+
+add_action( 'customize_controls_init', 'abraham_customize_css' );
+
+add_action( 'admin_print_styles', 'abraham_admin_styles' );
+
 /**
- * Enqueue Google Fonts Example
+ * Enqueue Google Fonts Example.
  */
 function abraham_custom_fonts() {
 
@@ -22,94 +29,90 @@ function abraham_custom_fonts() {
 	wp_enqueue_style( 'abraham_custom_fonts', $font_uri, array(), null, 'screen' );
 
 }
-add_action( 'wp_enqueue_scripts', 'abraham_custom_fonts' );
 
+/**
+ * Adds visual selectors for the layout option in the Theme Customizer.
+ */
+function abraham_customize_css() { ?>
 
+	<style type="text/css">
+		#customize-control-theme-layout-control input[value="1c"]:before {
+			content: "\f134";
+		}
+		#customize-control-theme-layout-control input[value="2c-l"]:before {
+			content: "\f135";
+		}
+		#customize-control-theme-layout-control input[value="2c-r"]:before {
+			content: "\f136";
+		}
+		#customize-control-theme-layout-control input[type=radio] {
+			font-family: dashicons;
+			font-size: 32px;
+		  	margin-right: 20px;
+			color: #ddd;
+			border: 0;
+			line-height: 0;
+			height: 0;
+			width: 0;
+		}
+		#customize-control-theme-layout-control input[type=radio]:checked:before {
+			color: #888;
+			text-indent: 0;
+			-webkit-border-radius: 0;
+			border-radius: 0;
+			font-size: 32px;
+			width: 0;
+			height: 0;
+			margin: 0;
+			line-height: 0;
+			background: none;
+		}
+	</style>
+<?php }
 
-
-
-
-
-
-function abraham_customize_css() {
-?>
-<style type="text/css">
-#customize-control-theme-layout-control input[value="1c"]:before {
-	content: "\f134";
-}
-#customize-control-theme-layout-control input[value="2c-l"]:before {
-	content: "\f135";
-}
-#customize-control-theme-layout-control input[value="2c-r"]:before {
-	content: "\f136";
-}
-#customize-control-theme-layout-control input[type=radio] {
-	font-family: dashicons;
-	font-size: 32px;
-  	margin-right: 20px;
-	color: #ddd;
-	border: 0;
-	line-height: 0;
-	height: 0;
-	width: 0;
-}
-#customize-control-theme-layout-control input[type=radio]:checked:before {
-	color: #888;
-	text-indent: 0;
-	-webkit-border-radius: 0;
-  border-radius: 0;
-  font-size: 32px;
-  width: 0;
-  height: 0;
-   margin: 0;
-  line-height: 0;
-  background: none;
-}
-</style>
-<?php
-}
-add_action( 'customize_controls_init', 'abraham_customize_css' );
+/**
+ * Adds visual selectors for the layout option in the Post Admin.
+ */
 function abraham_admin_styles() {
-?>
-<style type="text/css">
-#theme-layouts-post-meta-box input[value=default]:before {
-	content: "\f111";
+	?>
+	<style type="text/css">
+		#theme-layouts-post-meta-box input[value=default]:before {
+			content: "\f111";
+		}
+		#theme-layouts-post-meta-box input[value="1c"]:before {
+			content: "\f134";
+		}
+		#theme-layouts-post-meta-box input[value="2c-l"]:before {
+			content: "\f135";
+		}
+		#theme-layouts-post-meta-box input[value="2c-r"]:before {
+			content: "\f136";
+		}
+		#theme-layouts-post-meta-box input[type=radio] {
+			font-family: dashicons;
+			font-size: 32px;
+		  	margin-right: 20px;
+			color: #ddd;
+			border: 0;
+			line-height: 0;
+			height: 0;
+			width: 0;
+		}
+		#theme-layouts-post-meta-box input[type=radio]:checked:before {
+			color: #888;
+			text-indent: 0;
+			-webkit-border-radius: 0;
+			border-radius: 0;
+			font-size: 32px;
+			width: 0;
+			height: 0;
+			margin: 0;
+			line-height: 0;
+			background: none;
+		}
+		#theme-layouts-post-meta-box li {
+		  margin-bottom: 15px;
+		}
+	</style>
+	<?php
 }
-#theme-layouts-post-meta-box input[value="1c"]:before {
-	content: "\f134";
-}
-#theme-layouts-post-meta-box input[value="2c-l"]:before {
-	content: "\f135";
-}
-#theme-layouts-post-meta-box input[value="2c-r"]:before {
-	content: "\f136";
-}
-#theme-layouts-post-meta-box input[type=radio] {
-	font-family: dashicons;
-	font-size: 32px;
-  	margin-right: 20px;
-	color: #ddd;
-	border: 0;
-	line-height: 0;
-	height: 0;
-	width: 0;
-}
-#theme-layouts-post-meta-box input[type=radio]:checked:before {
-	color: #888;
-	text-indent: 0;
-	-webkit-border-radius: 0;
-  border-radius: 0;
-  font-size: 32px;
-  width: 0;
-  height: 0;
-   margin: 0;
-  line-height: 0;
-  background: none;
-}
-#theme-layouts-post-meta-box li {
-  margin-bottom: 15px;
-}
-</style>
-<?php
-}
-add_action( 'admin_print_styles', 'abraham_admin_styles' );
