bdad001fd5f232c9bd8ed1558bb2c6b40a7f791c
m-e-h <info@martyhelmick.com>
2015-03-04 13:10:06 -0500

    bem the menus a bit

--- menu/social.php
+++ menu/social.php
@@ -12,7 +12,7 @@ if ( has_nav_menu( 'social' ) ) :
 			'container_id'    => 'menu-social',
 			'container_class' => 'menu menu__social',
 			'menu_id'         => 'menu-social-items',
-			'menu_class'      => 'menu-items menu-items__social',
+			'menu_class'      => 'menu-items menu__social--items',
 			'depth'           => 1,
 			'link_before'     => '<span class="screen-reader-text">',
 			'link_after'      => '</span>',
