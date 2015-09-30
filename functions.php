c55ec2afd18ab5a8b44a5ea7198112cbd5154e2c
m-e-h <info@martyhelmick.com>
2015-09-30 19:07:59 -0400

    move slug function to functions

--- functions.php
+++ functions.php
@@ -67,3 +67,15 @@ function abraham_assets() {
         false, false, true
     );
 }
+
+function get_the_slug( $id=null ){
+    if( empty($id) ):
+        global $post;
+    if( empty($post) )
+        return ''; // No global $post var available.
+        $id = $post->ID;
+    endif;
+
+    $slug = basename( get_permalink($id) );
+    return $slug;
+}
--- inc/utils.php
+++ inc/utils.php
@@ -108,16 +108,3 @@ function doc_page_css_class( $css_class, $page ) {
 
 	return $css_class;
 }
-
-
-function get_the_slug( $id=null ){
-    if( empty($id) ):
-        global $post;
-    if( empty($post) )
-        return ''; // No global $post var available.
-        $id = $post->ID;
-    endif;
-
-    $slug = basename( get_permalink($id) );
-    return $slug;
-}
