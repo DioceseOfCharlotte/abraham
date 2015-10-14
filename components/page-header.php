77373143045e582a5e04364b20cef80a059de4e8
m-e-h <info@martyhelmick.com>
2015-10-14 09:59:14 -0400

    add archive description

--- components/page-header.php
+++ components/page-header.php
@@ -20,4 +20,11 @@
         }
         ?>
     </h1>
+    <?php if ( ! is_paged() && $desc = get_the_archive_description() ) : // Check if we're on page/1. ?>
+
+		<div <?php hybrid_attr( 'archive-description' ); ?>>
+			<?php echo $desc; ?>
+		</div><!-- .archive-description -->
+
+	<?php endif; // End paged check. ?>
 </div>
