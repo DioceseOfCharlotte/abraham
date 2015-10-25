50789f4ee77a38b11471a64698c68969f027f87d
m-e-h <info@martyhelmick.com>
2015-10-25 19:53:46 -0400

    correct path to images

--- components/section-toggles.php
+++ components/section-toggles.php
@@ -6,7 +6,7 @@ global $mehsc_atts;
 ?>
 
 <a class="collapse-toggle u-inline-block u-flexed-first u-text-center u-color-inherit" data-collapse="#section<?php the_ID(); ?>" data-group="accordion" href="#">
-    <?php get_template_part('images/icon', get_the_slug() ); ?>
+    <?php get_template_part('assets/images/icon', get_the_slug() ); ?>
     <h5><?php the_title(); ?></h5>
 </a>
 <div class="collapse u-overflow-hidden u-px3 u-mxn3 mui-enter slideInUp fast" id="section<?php the_ID(); ?>">
