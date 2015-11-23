dfe6620f6250b0e17f10e6050f26188ca8cf90e5
m-e-h <info@martyhelmick.com>
2015-11-23 13:50:47 -0500

    close tag

--- components/entry-footer.php
+++ components/entry-footer.php
@@ -1,6 +1,6 @@
 <?php if ( !is_singular( get_post_type() ) ) {
     return;
-}
+} ?>
 <footer class="entry-footer">
     <?php hybrid_post_terms( array( 'taxonomy' => 'category', 'text' => esc_html__( 'Posted in %s', 'abraham' ) ) ); ?>
     <?php hybrid_post_terms( array( 'taxonomy' => 'post_tag', 'text' => esc_html__( 'Tagged %s', 'abraham' ), 'before' => '<br />' ) ); ?>
