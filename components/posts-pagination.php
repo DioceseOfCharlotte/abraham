dc6009af1bc4e432f1a2858312e5088fb7529df6
m-e-h <info@martyhelmick.com>
2016-01-04 18:36:28 -0500

    only show pagination on archives

--- components/posts-pagination.php
+++ components/posts-pagination.php
@@ -1,10 +1,12 @@
-<div class="u-1/1 u-text-center">
+<?php if ( is_home() || is_archive() || is_search() ) : ?>
+    <div class="u-1/1 u-text-center">
 
-    <?php the_posts_pagination(
-        array(
-            'prev_text' => esc_html_x( '&larr; Previous', 'posts navigation', 'abraham' ),
-            'next_text' => esc_html_x( 'Next &rarr;',     'posts navigation', 'abraham' ),
-        )
-    ); ?>
+        <?php the_posts_pagination(
+            array(
+                'prev_text' => esc_html_x( '&larr; Previous', 'posts navigation', 'abraham' ),
+                'next_text' => esc_html_x( 'Next &rarr;',     'posts navigation', 'abraham' ),
+            )
+        ); ?>
 
-</div>
+    </div>
+<?php endif;
