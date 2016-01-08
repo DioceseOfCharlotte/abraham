5f7563d74f4b54efc1a0b33ce93110e8dc07b953
m-e-h <info@martyhelmick.com>
2016-01-08 18:01:09 -0500

    add new wp-core comments_navigaion

--- comments.php
+++ comments.php
@@ -12,24 +12,12 @@ if ( post_password_required() || ( !have_comments() && !comments_open() ) )
       <?php wp_list_comments(array('style' => 'ol', 'short_ping' => true)); ?>
     </ol>
 
-    <?php if (get_comment_pages_count() > 1 && get_option('page_comments')) : ?>
-      <nav>
-        <ul class="pager">
-          <?php if (get_previous_comments_link()) : ?>
-            <li class="previous"><?php previous_comments_link(__('&larr; Older comments', 'abraham')); ?></li>
-          <?php endif; ?>
-          <?php if (get_next_comments_link()) : ?>
-            <li class="next"><?php next_comments_link(__('Newer comments &rarr;', 'abraham')); ?></li>
-          <?php endif; ?>
-        </ul>
-      </nav>
-    <?php endif; ?>
+    <?php the_comments_navigation(); ?>
+
   <?php endif; // have_comments() ?>
 
   <?php if (!comments_open() && get_comments_number() != '0' && post_type_supports(get_post_type(), 'comments')) : ?>
-    <div class="alert alert-warning">
-      <?php _e('Comments are closed.', 'abraham'); ?>
-    </div>
+      <p class="no-comments"><?php esc_html_e( 'Comments are closed.', '_s' ); ?></p>
   <?php endif; ?>
 
   <?php comment_form(); ?>
