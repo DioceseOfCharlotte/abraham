8a9945300241f792bd2d5e635b1850c6f8ca8fba
m-e-h <info@martyhelmick.com>
2016-01-13 18:26:48 -0500

    link styles

---
+++ content/document.php
@@ -0,0 +1,28 @@
+
+
+    <article <?php hybrid_attr('post'); ?>>
+
+        <?php tha_entry_top(); ?>
+
+            <header <?php hybrid_attr('entry-header'); ?>>
+                <?php
+                    get_the_image(array(
+                        'size' => 'abraham-lg',
+                    ));
+                ?>
+                <h2 <?php hybrid_attr('entry-title'); ?>>
+                    <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+                </h2>
+            </header>
+
+            <div <?php hybrid_attr('entry-summary'); ?>>
+                <?php tha_entry_content_before(); ?>
+                <?php the_excerpt(); ?>
+                <?php tha_entry_content_after(); ?>
+            </div>
+
+            <?php get_template_part('components/entry', 'footer'); ?>
+
+    <?php tha_entry_bottom(); ?>
+
+    </article>
