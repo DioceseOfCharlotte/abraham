1c6e123075dd61c4b99e31f40fa16f8c0def301d
m-e-h <info@martyhelmick.com>
2016-01-15 12:13:22 -0500

    use cpt function and organize mataboxes

--- inc/archive-width.php
+++ inc/archive-width.php
@@ -1,25 +1,26 @@
 <?php
+
 add_action( 'init', 'archive_width_taxonomy' );
 
 // Register Custom Taxonomy
 function archive_width_taxonomy() {
 
-	register_taxonomy( 'archive_post_width',
-		array( 'post' ),
-		array(
-		'public' => true,
-		'hierarchical' => false,
-		'labels' => array(
-			'name' => _x( 'Width', 'post width' ),
-			'singular_name' => _x( 'Width', 'post width' ),
-		),
-		'query_var' => true,
-		'show_admin_column' => true,
-		//'rewrite' => false,
-		'show_ui' => true,
-		//'_builtin' => true,
-		'show_in_nav_menus' => false,
-	) );
+    register_taxonomy( 'archive_post_width',
+        get_non_hierarchy_cpts(),
+        array(
+        'public' => true,
+        'hierarchical' => false,
+        'labels' => array(
+            'name' => _x( 'Width', 'post width' ),
+            'singular_name' => _x( 'Width', 'post width' ),
+        ),
+        'query_var' => true,
+        'show_admin_column' => true,
+        //'rewrite' => false,
+        'show_ui' => true,
+        //'_builtin' => true,
+        'show_in_nav_menus' => false,
+    ) );
 }
 
 
@@ -29,62 +30,61 @@ add_action( 'cmb2_admin_init', 'rcdoc_register_metabox' );
  * Can only happen on the 'cmb2_admin_init' or 'cmb2_init' hook.
  */
 function rcdoc_register_metabox() {
-	$prefix = 'rcdoc_';
+    $prefix = 'doc_';
 
-	/**
-	 * Sample metabox to demonstrate each field type included
-	 */
-	$rcdoc_meta = new_cmb2_box( array(
-		'id'            => $prefix . 'metabox',
-		'title'         => __( 'Test Metabox', 'cmb2' ),
-		'object_types'  => array(
-			'post',
-			// 'page',
-			// 'bishop',
-			// 'cpt_archive',
-			// 'department',
-		),
-		'context'    => 'side',
-		'priority'   => 'high',
-		//'show_names' => false,
-	) );
+    /**
+     * Page Styles metabox.
+     */
+    $rcdoc_color_meta = new_cmb2_box( array(
+        'id'            => $prefix . 'metabox',
+        'title'         => __( 'Page Colors', 'cmb2' ),
+        'object_types'  => get_hierarchy_cpts(),
+        'context'    => 'side',
+        'priority'   => 'high',
+        //'show_names' => false,
+    ) );
 
-	$rcdoc_meta->add_field( array(
-		'name'    => __( 'Accent Color', 'cmb2' ),
-		'desc'    => __( 'Accent color used throughout the page.', 'cmb2' ),
-		'id'      => $prefix . 'colors',
-		'type'    => 'colorpicker',
-		'default' => '#ffffff',
-		'priority'   => 'high',
-		'attributes' => array(
-			'data-colorpicker' => json_encode( array(
-				'palettes' => array( '#e87', '#fc6', '#6d7', '#6ca', '#38e', '#a9d' ),
-			) ),
-		),
-	) );
+        $rcdoc_color_meta->add_field( array(
+            'name'    => __( 'Primary Color', 'cmb2' ),
+            'desc'    => __( 'Primary color used throughout the page.', 'cmb2' ),
+            'id'      => $prefix . 'page_primary_color',
+            'type'    => 'colorpicker',
+            'default' => apply_filters('theme_mod_primary_color', ''),
+            'priority'   => 'high',
+            'attributes' => array(
+                'data-colorpicker' => json_encode( array(
+                    'palettes' => array( '#e87', '#fc6', '#6d7', '#6ca', '#38e', '#a9d' ),
+                ) ),
+            ),
+        ) );
 
-	$rcdoc_meta->add_field( array(
-		'name'     => __( 'Archive Width', 'cmb2' ),
-		'desc'     => __( 'Seen on Landing Page', 'cmb2' ),
-		'id'       => $prefix . 'width_taxonomy_radio',
-		'type'     => 'taxonomy_radio',
-		'taxonomy' => 'archive_post_width', // Taxonomy Slug
-		//'inline'  => true, // Toggles display to inline
-	) );
+        $rcdoc_color_meta->add_field( array(
+            'name'    => __( 'Secondary Color', 'cmb2' ),
+            'desc'    => __( 'Secondary color used throughout the page.', 'cmb2' ),
+            'id'      => $prefix . 'page_secondary_color',
+            'type'    => 'colorpicker',
+            'default' => apply_filters('theme_mod_secondary_color', ''),
+            'priority'   => 'high',
+            'attributes' => array(
+                'data-colorpicker' => json_encode( array(
+                    'palettes' => array( '#e87', '#fc6', '#6d7', '#6ca', '#38e', '#a9d' ),
+                ) ),
+            ),
+        ) );
+
 }
 
 
 
-function get_archive_post_width( $post = null ) {
-	if ( ! $post = get_post( $post ) )
-		return false;
+function get_archive_post_width($post = null) {
+    if ( ! $post = get_post( $post ) )
+        return false;
 
-$_width = get_the_terms( $post->ID, 'archive_post_width' );
+    $_width = get_the_terms( $post->ID, 'archive_post_width' );
 
-if ( empty( $_width ) )
-	return false;
+    if ( empty( $_width ) )
+        return false;
 
-	$width = reset( $_width );
-
-return $width->slug;
+    $width = reset( $_width );
+        return $width->slug;
 }
