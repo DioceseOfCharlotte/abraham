8bdbc6b284bbbd7aed04693995bd2a5cc270e4a0
m-e-h <info@martyhelmick.com>
2016-02-15 15:49:31 -0500

    just show title on image format archive

--- content/image.php
+++ content/image.php
@@ -13,18 +13,12 @@
 			'image_class' => 'u-br u-block u-1of1',
 		));
 	?>
-<div class="u-flex u-flex-column u-flex-justify-end u-text-white u-px2 u-abs u-bottom0 u-left0 u-1of1 u-top0 u-color-inherit">
+<div class="u-flex u-flex-column u-flex-justify-end u-text-white u-abs u-bottom0 u-left0 u-1of1 u-top0 u-color-inherit">
 		<header <?php hybrid_attr('entry-header'); ?>>
 			<h2 <?php hybrid_attr('entry-title'); ?>>
 				<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
 			</h2>
 		</header>
-
-		<div <?php hybrid_attr('entry-summary'); ?>>
-			<?php tha_entry_content_before(); ?>
-			<?php the_excerpt(); ?>
-			<?php tha_entry_content_after(); ?>
-		</div>
 
 		<?php get_template_part('components/entry', 'footer'); ?>
 </div>
