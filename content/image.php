0176b05b002fc82139df730ba82e94b21f14f1f0
m-e-h <info@martyhelmick.com>
2016-04-06 12:17:55 -0400

    image overlay

--- content/image.php
+++ content/image.php
@@ -4,25 +4,26 @@
  *
  * @package abraham
  */
+
 ?>
-<section <?php hybrid_attr('post'); ?>>
+<section <?php hybrid_attr( 'post' ); ?>>
 	<?php
 		get_the_image(array(
 			'size' => 'abe-hd',
 			'link_to_post' => false,
 			'image_class' => 'u-br u-block u-1of1',
-			'default_image' => get_template_directory_uri() . '/images/blank.jpg'
+			'default_image' => get_template_directory_uri() . '/images/blank.jpg',
 		));
 	?>
 
-<div class="u-flex u-flex-column u-flex-justify-end u-text-white u-abs u-bottom0 u-left0 u-1of1 u-top0 u-color-inherit">
-		<header <?php hybrid_attr('entry-header'); ?>>
-			<h2 <?php hybrid_attr('entry-title'); ?>>
+<div class="u-flex u-flex-column u-flex-justify-end u-text-white u-abs u-bottom0 u-left0 u-1of1 u-top0 u-grad-overlay u-color-inherit">
+		<header <?php hybrid_attr( 'entry-header' ); ?>>
+			<h2 <?php hybrid_attr( 'entry-title' ); ?>>
 				<?php the_title(); ?>
 			</h2>
 		</header>
 
-		<?php get_template_part('components/entry', 'footer'); ?>
+		<?php get_template_part( 'components/entry', 'footer' ); ?>
 </div>
 <a class="u-1of1 u-abs u-bottom0 u-z1 u-top0 u-left0" href="<?php the_permalink(); ?>"></a>
 </section>
