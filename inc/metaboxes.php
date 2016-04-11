5f66dab083eb48c6c906cc35e56e502a61a21aed
m-e-h <info@martyhelmick.com>
2016-04-11 18:55:09 -0400

    straighten up

--- inc/metaboxes.php
+++ inc/metaboxes.php
@@ -22,27 +22,27 @@ function abe_register_metaboxes() {
 		'show_names' => false,
 	) );
 
-		$abe_color_meta->add_field( array(
-			'name'       => __( 'Primary Color', 'cmb2' ),
-			'id'         => $prefix . 'page_primary_color',
-			'type'       => 'colorpicker',
-			'default'    => apply_filters( 'theme_mod_primary_color', '' ),
-			'attributes' => array(
-				'data-colorpicker' => json_encode( array(
-					'palettes' => array( '#34495E', '#2980b9', '#39CCCC', '#16a085', '#FFC107', '#F44336' ),
-				) ),
-			),
-		) );
+	$abe_color_meta->add_field( array(
+		'name'       => __( 'Primary Color', 'cmb2' ),
+		'id'         => $prefix . 'page_primary_color',
+		'type'       => 'colorpicker',
+		'default'    => apply_filters( 'theme_mod_primary_color', '' ),
+		'attributes' => array(
+			'data-colorpicker' => wp_json_encode( array(
+				'palettes' => array( '#34495E', '#2980b9', '#39CCCC', '#16a085', '#FFC107', '#F44336' ),
+			) ),
+		),
+	) );
 
-			$abe_color_meta->add_field( array(
-				'name'       => __( 'Secondary Color', 'cmb2' ),
-				'id'         => $prefix . 'page_secondary_color',
-				'type'       => 'colorpicker',
-				'default'    => apply_filters( 'theme_mod_secondary_color', '' ),
-				'attributes' => array(
-					'data-colorpicker' => json_encode( array(
-						'palettes' => array( '#34495E', '#2980b9', '#39CCCC', '#16a085', '#FFC107', '#F44336' ),
-					) ),
-				),
-			) );
+	$abe_color_meta->add_field( array(
+		'name'       => __( 'Secondary Color', 'cmb2' ),
+		'id'         => $prefix . 'page_secondary_color',
+		'type'       => 'colorpicker',
+		'default'    => apply_filters( 'theme_mod_secondary_color', '' ),
+		'attributes' => array(
+			'data-colorpicker' => wp_json_encode( array(
+				'palettes' => array( '#34495E', '#2980b9', '#39CCCC', '#16a085', '#FFC107', '#F44336' ),
+			) ),
+		),
+	) );
 }
