dac13f376a8d092db845a940534ee42de1bc0f0a
m-e-h <info@martyhelmick.com>
2016-04-22 19:08:52 -0400

    little more top padding on entries

--- inc/attr-trumps.php
+++ inc/attr-trumps.php
@@ -55,16 +55,16 @@ class Attr_Trumps {
 
 			'page_header'             	=> 'u-p4 u-1of1 u-rel u-text-center',
 			'page_title'              	=> 'u-text-shadow u-h0 u-m0 u-pb1',
-			'archive_description'     	=> 'u-1of1 u-p3 u-text-left u-br u-container u-mb3 u-bg-white u-rel u-shadow--3dp',
+			'archive_description'     	=> 'u-1of1 u-p3 u-py4-md u-text-left u-br u-container u-mb3 u-bg-white u-rel u-shadow--3dp',
 
 			// ENTRY.
-			'post'                    	=> 'u-fit o-cell u-bg-white u-mb3 u-1of1 u-br',
+			'post'                    	=> 'u-fit o-cell u-bg-white u-mb3 u-1of1 u-br u-shadow--3dp',
 			'post_wide'               	=> 'u-fit u-1of1',
 
 			'post_archive'            	=> 'u-mb3 o-cell u-1of2-md u-bg-white u-br',
 			'entry_header'            	=> 'u-z1',
 			'entry_title'             	=> 'u-px3 u-py2 u-h3 u-m0 u-flexed-auto',
-			'entry_content'           	=> 'u-p3',
+			'entry_content'           	=> 'u-p3 u-py4-md',
 			'entry_content_wide'      	=> '',
 			'entry_summary'           	=> 'u-px3 u-mb2',
 			'entry_footer'            	=> 'u-mt-auto',
