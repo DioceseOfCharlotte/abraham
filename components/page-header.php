d6af6eaa1624ee7678a1cdaeb7ea3ae667ebc920
m-e-h <info@martyhelmick.com>
2016-04-26 19:33:17 -0400

    semantic archive description

--- components/page-header.php
+++ components/page-header.php
@@ -1,30 +1,30 @@
-<?php if (is_home() || is_front_page()) {
+<?php if ( is_home() || is_front_page() ) {
 	return;
 }
 ?>
 
-<div <?php hybrid_attr('archive-header'); ?>>
+<div <?php hybrid_attr( 'archive-header' ); ?>>
 
-	<?php hybrid_get_menu('breadcrumbs'); ?>
+	<?php hybrid_get_menu( 'breadcrumbs' ); ?>
 
-	<h1 <?php hybrid_attr('archive-title'); ?>>
+	<h1 <?php hybrid_attr( 'archive-title' ); ?>>
 		<?php
-		if (is_archive()) {
+		if ( is_archive() ) {
 			echo get_the_archive_title();
-		} elseif (is_search()) {
-			echo sprintf(esc_html__('Search Results for %s', 'abraham'), get_search_query());
-		} elseif (is_404()) {
-			echo esc_html__('Not Found', 'abraham');
-		} elseif (!hybrid_is_plural()) {
+		} elseif ( is_search() ) {
+			echo sprintf( esc_html__( 'Search Results for %s', 'abraham' ), get_search_query() );
+		} elseif ( is_404() ) {
+			echo esc_html__( 'Not Found', 'abraham' );
+		} elseif ( ! hybrid_is_plural() ) {
 			echo get_the_title();
 		}
 		?>
 	</h1>
 	<?php if ( ! is_paged() && $desc = get_the_archive_description() ) : // Check if we're on page/1. ?>
 
-		<div <?php hybrid_attr( 'archive-description' ); ?>>
+		<article <?php hybrid_attr( 'archive-description' ); ?>>
 			<?php echo $desc; ?>
-		</div><!-- .archive-description -->
+		</article><!-- .archive-description -->
 
 	<?php endif; // End paged check. ?>
 </div>
