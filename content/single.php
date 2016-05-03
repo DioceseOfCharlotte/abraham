1856f6c9660c6a664437fee5b9929f5f853c4fa2
m-e-h <info@martyhelmick.com>
2016-05-03 19:11:11 -0400

    gravity-view always shows empty.

--- content/single.php
+++ content/single.php
@@ -5,9 +5,9 @@
  * @package abraham
  */
 
-if ( ! hybrid_post_has_content() ) {
-	return;
-}
+// if ( ! hybrid_post_has_content() ) {
+// 	return;
+// }
 ?>
 
 <article <?php hybrid_attr( 'post' ); ?>>
