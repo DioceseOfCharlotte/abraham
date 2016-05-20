72ca65c7c9886d3bd9d1166171414ad10a681e23
m-e-h <info@martyhelmick.com>
2016-05-20 18:21:26 -0400

    add default thumb and template parts for get_the_image

---
+++ components/img-hd.php
@@ -0,0 +1,8 @@
+<?php
+
+get_the_image(array(
+	'size' 				=> 'abe-card-md',
+	'image_class' 		=> 'u-br-t u-1of1',
+	'before'   			=> '<div class="card-img u-overflow-hidden">',
+	'after' 			=> '</div>',
+));
---
+++ components/img-thumb.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Returns the parent theme editor stylesheet URI.
+ *
+ * @since  2.2.0
+ * @access public
+ * @return string
+ */
+
+get_the_image(array(
+	'image_class' => 'u-br-t u-1of1',
+	'default_image' => abe_get_default_image(),
+	'before'             => '<div class="card-img u-overflow-hidden">',
+	'after'              => '</div>',
+));
