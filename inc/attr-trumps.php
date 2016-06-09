785844b11603971fc2618e48752371482f1d756e
m-e-h <info@martyhelmick.com>
2016-06-09 15:32:11 -0400

    IE needs a defined width rather than just a max

--- inc/attr-trumps.php
+++ inc/attr-trumps.php
@@ -42,7 +42,7 @@ class Attr_Trumps {
 			'site_container'         	  => '',
 			'layout'                 	  => 'u-flex u-flex-col u-vh100-min',
 			'layout_wide'            	  => '',
-			'grid'                   	  => 'o-grid u-container  u-rel',
+			'grid'                   	  => 'o-grid u-container u-rel',
 			'grid_1-wide'               => 'o-grid u-1of1  u-rel u-p0',
 			'grid_2c-r'                 => 'o-grid u-container  u-rel u-flex-rev',
 			'grid_2c-l'                 => 'o-grid u-container u-rel',
