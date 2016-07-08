4b1dfe952360df69621f842250b11e3bf22684a3
m-e-h <info@martyhelmick.com>
2016-07-08 18:51:16 -0400

    style 404 a bit

--- content/404.php
+++
@@ -1,9 +0,0 @@
-<article <?php hybrid_attr( 'post' ); ?>>
-
-	<div <?php hybrid_attr( 'entry-content' ); ?>>
-		<?php _e( 'Sorry, but the page you were trying to view does not exist. Perhaps searching can help.', 'abraham' ); ?>
-	</div>
-
-	<?php get_search_form(); ?>
-
-</article>
--- content/none.php
+++ content/none.php
@@ -6,19 +6,19 @@
  */
 
 ?>
-<article class="u-fit o-cell u-bg-white u-mb3 u-1of1 u-br u-shadow2">
+<article class="u-fit o-cell u-bg-white u-text-center u-mb3 u-1of1 u-br u-shadow2">
 
 	<div <?php hybrid_attr( 'entry-content' ); ?>>
 
 		<?php if ( is_search() ) : ?>
 
 			<p><?php esc_html_e( 'Sorry, but nothing matched your search. Perhaps try again with some different keywords.', 'abraham' ); ?></p>
-			<?php get_search_form(); ?>
+			<p class="u-1of1"><?php get_search_form(); ?></p>
 
 		<?php else : ?>
 
-			<p><?php esc_html_e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'abraham' ); ?></p>
-			<?php get_search_form(); ?>
+			<p><?php esc_html_e( 'The page you were looking for isn\'t here. Perhaps searching can help.', 'abraham' ); ?></p>
+			<div class="u-1of1"><?php get_search_form(); ?></div>
 
 		<?php endif; ?>
 
