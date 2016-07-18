5a3d3bdaa633a8a25c24af7275fd7ad08a021396
m-e-h <info@martyhelmick.com>
2016-07-18 13:48:11 -0400

    design search results

--- content/search.php
+++ content/search.php
@@ -6,23 +6,23 @@
  */
 
 ?>
-<article <?php hybrid_attr( 'post' ); ?>>
+<article class="u-1of1 u-overflow-hidden u-text-wrap u-bg-white u-br u-shadow1 u-mb2 u-flex u-flex-wrap">
 
 	<?php tha_entry_top(); ?>
 
 	<?php get_template_part( 'components/img', 'thumb' ); ?>
 
-	<div class="flag-body u-flexed-auto">
+	<div class="flag-body u-flexed-1">
 
-		<?php get_template_part( 'components/entry', 'header' ); ?>
+		<h2 class="u-f-plus u-m0 u-p0">
+			<a class="u-inline-flex u-link u-1of1 u-p2 u-pb1 u-flex-jb" href="<?php the_permalink(); ?>"><?php the_title(); ?><?php abe_do_svg( 'arrow-right', 'sm' ) ?></a>
+		</h2>
 
-		<?php if ( has_excerpt() ) { ?>
-		<div <?php hybrid_attr( 'entry-summary' ); ?>>
+		<div class="entry-summary u-p2 show-icons">
 			<?php tha_entry_content_before(); ?>
 			<?php the_excerpt(); ?>
 			<?php tha_entry_content_after(); ?>
 		</div>
-		<?php } ?>
 
 	</div>
 
