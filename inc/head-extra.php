faab3b7779fe2b3e01502e861e84a739f60d94b6
m-e-h <info@martyhelmick.com>
2016-07-18 17:24:18 -0400

    less aggresive removing head stuff

--- inc/head-extra.php
+++ inc/head-extra.php
@@ -8,31 +8,31 @@
 function abe_head_cleanup() {
 	// Originally from http://wpengineer.com/1438/wordpress-header/
 	remove_action( 'wp_head', 'feed_links_extra', 3 );
-	add_action( 'wp_head', 'ob_start', 1, 0 );
-	add_action('wp_head', function () {
-		$pattern = '/.*' . preg_quote( esc_url( get_feed_link( 'comments_' . get_default_feed() ) ), '/' ) . '.*[\r\n]+/';
-		echo preg_replace( $pattern, '', ob_get_clean() );
-	}, 3, 0);
+	// add_action( 'wp_head', 'ob_start', 1, 0 );
+	// add_action('wp_head', function () {
+	// 	$pattern = '/.*' . preg_quote( esc_url( get_feed_link( 'comments_' . get_default_feed() ) ), '/' ) . '.*[\r\n]+/';
+	// 	echo preg_replace( $pattern, '', ob_get_clean() );
+	// }, 3, 0);
 	remove_action( 'wp_head', 'rsd_link' );
 	remove_action( 'wp_head', 'wlwmanifest_link' );
-	remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );
+	// remove_action( 'wp_head', 'adjacent_posts_rel_link_wp_head', 10 );
 	remove_action( 'wp_head', 'wp_generator' );
-	remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
+	// remove_action( 'wp_head', 'wp_shortlink_wp_head', 10 );
 	remove_action( 'wp_head', 'print_emoji_detection_script', 7 );
 	remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
 	remove_action( 'wp_print_styles', 'print_emoji_styles' );
 	remove_action( 'admin_print_styles', 'print_emoji_styles' );
-	remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
-	remove_action( 'wp_head', 'wp_oembed_add_host_js' );
-	remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
+	// remove_action( 'wp_head', 'wp_oembed_add_discovery_links' );
+	// remove_action( 'wp_head', 'wp_oembed_add_host_js' );
+	// remove_action( 'wp_head', 'rest_output_link_wp_head', 10 );
 	remove_filter( 'the_content_feed', 'wp_staticize_emoji' );
 	remove_filter( 'comment_text_rss', 'wp_staticize_emoji' );
 	remove_filter( 'wp_mail', 'wp_staticize_emoji_for_email' );
-	add_filter( 'use_default_gallery_style', '__return_false' );
-	global $wp_widget_factory;
-	if ( isset( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'] ) ) {
-		remove_action( 'wp_head', [ $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ] );
-	}
+	// add_filter( 'use_default_gallery_style', '__return_false' );
+	// global $wp_widget_factory;
+	// if ( isset( $wp_widget_factory->widgets['WP_Widget_Recent_Comments'] ) ) {
+	// 	remove_action( 'wp_head', [ $wp_widget_factory->widgets['WP_Widget_Recent_Comments'], 'recent_comments_style' ] );
+	// }
 
 	// hybrid meta
 	remove_action( 'wp_head', 'hybrid_meta_charset',   0 );
