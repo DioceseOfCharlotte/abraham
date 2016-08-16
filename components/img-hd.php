5926c32496303be6bf7a17dca6861bc2c2db2e0a
m-e-h <info@martyhelmick.com>
2016-08-16 18:55:33 -0400

    update image sizes

--- components/img-hd.php
+++ components/img-hd.php
@@ -1,7 +1,7 @@
 <?php
 
 get_the_image(array(
-	'size' 				=> 'abe-card-md',
+	'size' 				=> 'abe-hd',
 	'image_class' 		=> 'u-1of1 o-crop__content',
 	'before'   			=> '<div class="card-img u-overflow-hidden o-crop o-crop--16x9">',
 	'after' 			=> '</div>',
