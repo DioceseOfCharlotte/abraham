5d4ae0358edd7d8e30676ec75001b566470e2f76
m-e-h <info@martyhelmick.com>
2016-10-10 13:55:59 -0400

    no link on image post

--- components/img-hd.php
+++ components/img-hd.php
@@ -6,4 +6,5 @@ get_the_image(array(
 	'before'   			=> '<div class="card-img u-overflow-hidden o-crop o-crop--16x9">',
 	'after' 			=> '</div>',
 	'attachment' 		=> false,
+	'link_to_post' 		=> false,
 ));
