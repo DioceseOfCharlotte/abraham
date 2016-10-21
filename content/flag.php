8b1cd973ba4266b7a1ccbcffff84d2fd26a69166
m-e-h <info@martyhelmick.com>
2016-10-21 13:02:30 -0400

    unused footer

--- content/flag.php
+++ content/flag.php
@@ -16,7 +16,7 @@
 		<?php tha_entry_content_before(); ?>
 		<?php arch_excerpt(); ?>
 		<?php tha_entry_content_after(); ?>
-		<?php get_template_part( 'components/entry', 'footer' ); ?>
+	<?php edit_post_link('<svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 24 24"><path d="M3 17.25V21h3.75L17.81 9.94l-3.75-3.75L3 17.25zM20.71 7.04c.39-.39.39-1.02 0-1.41l-2.34-2.34c-.39-.39-1.02-.39-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"/></svg>'); ?>
 		<?php tha_entry_bottom(); ?>
 	</div>
 
