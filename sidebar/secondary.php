3168ed257c9cfac17b00c0df73ceef3cd1c36892
m-e-h <info@martyhelmick.com>
2016-12-03 14:15:02 -0500

    sidebar button color

--- sidebar/secondary.php
+++ sidebar/secondary.php
@@ -16,7 +16,7 @@ if ( ! is_active_sidebar( 'secondary' ) ) {
 
 		<header class="side-nav__header u-flex u-flex-wrap u-bg-2-light u-shadow1">
 			<div class="side-nav__title u-1of1 u-flex u-bg-2 u-flex-center u-p1 u-mb1">
-				<button class="js-menu-hide side-nav__hide u-z1 btn-round u-h3 u-inline-flex">
+				<button class="js-menu-hide side-nav__hide u-z1 btn-round u-bg-2-light u-h3 u-inline-flex">
 					<?php abe_do_svg( 'arrow-left', 'sm' ); ?>
 				</button>
 				<h2 class="u-h4 u-m0 u-text-display u-inline-block u-flexed-auto u-text-center"><a class="opacity-hover" href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h2>
