0bbd0f95c70b80d8ed438ef9e143d982d712e6c2
m-e-h <info@martyhelmick.com>
2016-12-27 12:43:44 -0500

    better fonts and font styles

--- inc/font-loader.php
+++ inc/font-loader.php
@@ -12,16 +12,16 @@ add_action( 'wp_head', 'abe_display_font' );
 */
 function abe_font_scripts() {
 
-wp_enqueue_style( 'abe_google_font', 'https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,500,600,700|Source+Sans+Pro:300,400,400i,600,600i,700' );
+	wp_enqueue_style( 'abe_google_font', 'https://fonts.googleapis.com/css?family=Cormorant+Garamond:400,400i,500,600,600i|Open+Sans:300,400,400i,600,700' );
 
-wp_enqueue_style( 'abe_font_awesome', 'https://use.fontawesome.com/1397c1e607.css' );
+	wp_enqueue_style( 'abe_font_awesome', 'https://use.fontawesome.com/1397c1e607.css' );
 
-wp_enqueue_script(
-	'font_face',
-	trailingslashit( get_template_directory_uri() ) . 'js/vendors/fontfaceobserver.js',
-	false, false, true
-);
-wp_add_inline_script( 'font_face', 'var fontA = new FontFaceObserver("Cormorant Garamond");var fontB = new FontFaceObserver("Source Sans Pro");fontA.load().then(function () {document.documentElement.className += " fontA";});fontB.load().then(function () {document.documentElement.className += " fontB";});' );
+	wp_enqueue_script(
+		'font_face',
+		trailingslashit( get_template_directory_uri() ) . 'js/vendors/fontfaceobserver.js',
+		false, false, true
+	);
+	wp_add_inline_script( 'font_face', 'var fontA = new FontFaceObserver("Cormorant Garamond");var fontB = new FontFaceObserver("Open Sans");fontA.load().then(function () {document.documentElement.className += " fontA";});fontB.load().then(function () {document.documentElement.className += " fontB";});' );
 
 }
 
@@ -29,7 +29,6 @@ function abe_display_font() {
 	$font_dir = trailingslashit( get_template_directory_uri() ) . 'fonts/'; ?>
 
 	<link rel="preload" href="<?php echo $font_dir ?>cormorant-garamond-regular.woff2" as="font" type="font/woff2" crossorigin>
-	<link rel="preload" href="<?php echo $font_dir ?>SourceSansPro-Regular.woff2" as="font" type="font/woff2" crossorigin>
 
 	<style type="text/css">
 		@font-face {
@@ -39,27 +38,16 @@ function abe_display_font() {
 			src:url('<?php echo $font_dir ?>cormorant-garamond-regular.woff2') format('woff2'),
 				url('<?php echo $font_dir ?>cormorant-garamond-regular.woff') format('woff');
 		}
-		@font-face {
-		    font-family: 'Source Sans ProFB';
-		    src: url('<?php echo $font_dir ?>SourceSansPro-Regular.woff2') format('woff2'),
-		         url('<?php echo $font_dir ?>SourceSansPro-Regular.woff') format('woff');
-		    font-weight: 400;
-		    font-style: normal;
-
-		}
-		body, .u-text-read {
-			font-family: "Source Sans ProFB", sans-serif;
-			font-weight: 400;
-		}
 		.u-text-display,.u-dropcap::first-letter {
 			font-family: CormorantFB, serif;
 			font-weight: 400;
 		}
 		.fontB body, .fontB .u-text-read {
-			font-family: "Source Sans Pro", sans-serif;
+			font-family: 'Open Sans', sans-serif;
 		}
 		.fontA .u-text-display,.fontA .u-dropcap::first-letter {
 			font-family: "Cormorant Garamond", serif;
+			font-weight: 600;
 		}
 	</style>
 <?php }
