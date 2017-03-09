4768ff82149a5ac3a63cdad41c175ccaa550a854
m-e-h <info@martyhelmick.com>
2017-03-09 14:13:13 -0500

    version bump

--- gulpfile.js
+++ gulpfile.js
@@ -36,7 +36,7 @@ const BANNER = [
 	'Author: Marty Helmick',
 	'Author URI: https://github.com/m-e-h',
 	'Description: Abraham is a Parent theme with many children.',
-	'Version: 0.9.6',
+	'Version: 0.9.7',
 	'License: GNU General Public License v2 or later',
 	'License URI: http://www.gnu.org/licenses/gpl-2.0.html',
 	'Text Domain: abraham',
