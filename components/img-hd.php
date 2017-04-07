f4b5819869eaf84c233782380af4175d1e450492
m-e-h <info@martyhelmick.com>
2017-04-07 16:35:35 -0400

    firefox doesn't like the o-crop

--- components/img-hd.php
+++ components/img-hd.php
@@ -4,11 +4,11 @@
 
 
 if ( has_post_thumbnail() ) : ?>
-<picture class="card-img u-overflow-hidden o-crop o-crop--16x9">
+<picture class="card-img u-overflow-hidden">
 	<?php echo abe_get_picture_source( get_the_ID(),
 		array(
 		'size'   => 'abe-hd',
-		'class'   => 'u-1of1 o-crop__content',
+		'class'   => 'u-1of1',
 		'thumb_url' => get_the_post_thumbnail_url( get_the_ID(), 'abe-hd' ),
 	)); ?>
 </picture>
