7c7fcd03ee818e8e81cd42d1a726692bb217293b
m-e-h <info@martyhelmick.com>
2018-02-14 16:14:04 -0500

    default value for abe_hint

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -142,7 +142,7 @@ function abe_has_layout( $layout ) {
 	return $layout === hybrid_get_theme_layout( 'theme_layout' );
 }
 
-function abe_hint( $text, $position ) {
+function abe_hint( $text, $position = 0 ) {
 	$position = $position ? "hint--{$position}" : 'hint--top-left';
 	$tooltip = '<span class="abe-tip ' . $position . '" aria-label="' . $text . '"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" viewBox="0 0 24 24"><path fill="none" d="M0 0h24v24H0z"/><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2zm1 16h-2v-2h2v2zm0-4.14V15h-2v-2c0-.552.448-1 1-1 1.103 0 2-.897 2-2s-.897-2-2-2-2 .897-2 2H8c0-2.21 1.79-4 4-4s4 1.79 4 4c0 1.862-1.278 3.413-3 3.86z"/></svg></span>';
 
